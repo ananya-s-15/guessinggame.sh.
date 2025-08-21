ea3c57ac5287d51c7d0ea07c4a08401144cdeef1
Ananya <ananyapsinha@gmail.com>
2025-08-21 21:46:23 +0530

    guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,28 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+
+# Function to get number of files
+function file_count {
+    echo $(ls -1 | wc -l)
+}
+
+correct=$(file_count)
+
+echo "Welcome to the Guessing Game!"
+echo "How many files are in the current directory?"
+
+while true
+do
+    read -p "Enter your guess: " guess
+    if [[ $guess -eq $correct ]]
+    then
+        echo "🎉 Congratulations! You guessed correctly!"
+        break
+    elif [[ $guess -lt $correct ]]
+    then
+        echo "Too low! Try again."
+    else
+        echo "Too high! Try again."
+    fi
+done
 
